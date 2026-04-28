ce72af3797b739d7e3c6f9b87711854fdcba3ce9
gmrdavid <gliamaeriodavid9@gmail.com>
2026-04-28 18:45:14 +0800

    auto-commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-});
+}); 
 
 db.connect(err => {
     if (err) throw err;
