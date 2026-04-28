ea63837e34e1dc8da128202b4ee1371ee80885df
gmrdavid <gliamaeriodavid9@gmail.com>
2026-04-28 19:08:36 +0800

    commit

--- server.js
+++ server.js
@@ -310,8 +310,6 @@ app.get('/api/admin/users', authenticateToken, isAdmin, (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
