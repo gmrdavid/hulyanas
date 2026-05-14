d64b3c6b8939088243f9fd4342ac11783bf3833c
gmrdavid <gliamaeriodavid9@gmail.com>
2026-05-15 03:04:27 +0800

    commit

--- server.js
+++ server.js
@@ -9,11 +9,6 @@ const path = require('path');
 const multer = require('multer');
 const fs = require('fs').promises;
 
-const { Pool } = require('pg');
-const pool = new Pool({connectionString: process.env.DB_NAME,
-    ssl: { rejectUnauthorized: false }
-});
-
 const app = express();
 const PORT = process.env.PORT;
 
