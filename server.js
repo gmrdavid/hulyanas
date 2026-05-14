c09ff6c07cd26148d426844a32ef37d13cd69d2c
gmrdavid <gliamaeriodavid9@gmail.com>
2026-05-14 16:04:42 +0800

    commit

--- server.js
+++ server.js
@@ -659,18 +659,7 @@ app.get('/api/analytics', authenticateToken, isAdmin, async (req, res) => {
         // =========================
         // FILTERS
         // =========================
-        app.get('/api/analytics', authenticateToken, isAdmin, async (req, res) => {
-    let conn;
-
-    try {
-        const { days = 'all', status = 'all' } = req.query;
-
-        conn = await pool.getConnection();
-
-        // =========================
-        // FILTERS
-        // =========================
-        let whereClause = `WHERE LOWER(o.status) != 'cancelled'`;
+        let whereClause = `WHERE LOWER(o.status) != 'cancelled' and LOWER(o.status) != 'pending'`;
         const params = [];
 
         if (days !== 'all') {
