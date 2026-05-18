1cb73713c9ba092b2865e4c1b87f2aa33552e5ce
gmrdavid <gliamaeriodavid9@gmail.com>
2026-05-19 05:29:17 +0800

    auto-commit

--- admin/js/dashboard.js
+++ admin/js/dashboard.js
@@ -415,7 +415,7 @@ async function loadActivityFeed(page = 1) {
                         </div>
                         <div class="activity-content">
                             <h4>${activity.message}</h4>
-                            <p class="activity-time">${formatDate(rawDate)}</p>
+                            <p class="activity-time">${formatDateTime(rawDate)}</p>
                         </div>
                     </div>
                 `;
