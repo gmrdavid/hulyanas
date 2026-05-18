6bdeb2eef5bd4b85583c82aabbebba9264416656
gmrdavid <gliamaeriodavid9@gmail.com>
2026-05-18 16:29:11 +0800

    commit

--- server.js
+++ server.js
@@ -1134,7 +1134,7 @@ app.get('/api/admin/activity', authenticateToken, isAdmin, async (req, res) => {
     try {
 
         const page = parseInt(req.query.page) || 1;
-        const limit = 10;
+        const limit = 9;
         const offset = (page - 1) * limit;
 
         // COUNT
